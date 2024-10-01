fda8d12ecea07add7323f937c4ee714964d39d36
pedroCMF <pedrounib@gmail.com>
2024-10-01 13:00:18 -0300

    segundo comit

--- Projeto PedroSandro/obj/Release/net8.0/Projeto PedroSandro.AssemblyInfo.cs
+++ Projeto PedroSandro/obj/Release/net8.0/Projeto PedroSandro.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto PedroSandro")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ef9664e9df7781edf2bf3aa47be74c2db832df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto PedroSandro")]
